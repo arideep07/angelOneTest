142a9bff008af84a7fbd346d355b3e33fe856a70
Arideep.Dutta <arideep.dutta@22B-MACABL-0257.local>
2023-09-27 13:49:29 +0530

    Added constants

--- constants/routes.go
+++ constants/routes.go
@@ -2,14 +2,7 @@ package constants
 
 // Route constants
 const (
-	SwaggerRoute          = "/swagger/*any"
-	ActuatorRoute         = "/actuator/*any"
-	FullNameRoute         = "/fullName"
-	MoxyRoute             = "/moxy"
-	CreateCounterRoute    = "/counter/create"
-	IncrementCounterRoute = "/counter/increment"
-	DecrementCounterRoute = "/counter/decrement"
-	CurrentCountRoute     = "/counter/current"
-	MathsSubmitJobRoute            = "/maths"
-	MathsGetJobStatusRoute            = "/maths/:jobid"
+	SwaggerRoute  = "/swagger/*any"
+	ActuatorRoute = "/actuator/*any"
+	LoggerRoute   = "/logger/*any"
 )
