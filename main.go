7b69339d7bed70fa75bf0e9ad1a10dd7f2b01b68
GirishBhat-M <girish.bhat@angelbroking.com>
2023-09-29 11:53:26 +0530

    update

--- main.go
+++ main.go
@@ -7,12 +7,19 @@ import (
 	"github.com/angel-one/go-utils/log"
 	"github.com/angel-one/go-utils/middlewares"
 	"github.com/arideep07/angelOneTest/api"
+	"github.com/arideep07/angelOneTest/constants"
 	"github.com/arideep07/angelOneTest/utils/flags"
 )
 
 func main() {
+	//set up logger
+	startLogger()
 	// Start the HTTP server and listen on port
 	startRouter()
+}
+
+func startLogger() {
+	log.InitLogger(log.Level(constants.InfoLevel))
 }
 
 func startRouter() {
